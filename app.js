a58ca9cb52387a1f91546d21267825562784fa34
joshuashea <joshshea44@gmail.com>
2023-07-26 15:59:52 -0700

    modified random image, added lighthouse score

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 'use-strict';
+const products = load();
+let clicks = 0;
+let maxClicksAllowed = 25;
+let lastImages = [];
 
 let productContainer = document.querySelector('section');
 let resultButton = document.querySelector('section + button');
@@ -7,21 +11,12 @@ let image2 = document.querySelector('section img:nth-child(2)');
 let image3 = document.querySelector('section img:nth-child(3)');
 let chartCanvas = document.getElementById('myChart');
 
-
-let clicks = 0;
-let maxClicksAllowed = 5;
-let lastImages = [];
-const products = [];
-
-
 function Product(name, src) {
   this.name = name;
   this.src = src;
   this.timesClicked = 0;
   this.timesSeen = 0;
-  products.push(this);
 }
-
 
 function getRandomNum() {
   return Math.floor(Math.random() * products.length);
@@ -32,31 +27,18 @@ function renderProducts() {
   let product2 = getRandomNum();
   let product3 = getRandomNum();
 
-  
-
-  
-  while ((product1 === product2 || product1 === product3)) {
+  while (product1 === product2 || product1 === product3 || lastImages.includes(product1)) {
     product1 = getRandomNum();
   }
-  while ((product2 === product3 || product2 === product1)) {
+  while (product2 === product3 || product2 === product1 || lastImages.includes(product2)) {
     product2 = getRandomNum();
   }
- 
-  // if (lastImages < 3) {
-  //   while (lastImages.includes(product1)) {
-  //     product1 = getRandomNum();
-  //   }
-  //   while(lastImages.includes(product2) || product2 === product1) {
-  //     product2 = getRandomNum();
-  //   }
-  //   while (lastImages.includes(product3) || product3 === product1 || product3 === product2) {
-  //     product3 = getRandomNum();
-  //   }
-  // }
-
-  // lastImages[0] = product1;
-  // lastImages[1] = product2;
-  // lastImages[2] = product3;
+  while (product3 === product1 || product3 === product2 || lastImages.includes(product3)) {
+    product3 = getRandomNum();
+  }
+  lastImages[0] = (product1);
+  lastImages[1] = (product2);
+  lastImages[2] = (product3);
 
   image1.src = products[product1].src;
   image2.src = products[product2].src;
@@ -80,7 +62,6 @@ function handleProductClick(event) {
   for (let i = 0; i < products.length; i++) {
     if (clickProduct === products[i].name) {
       products[i].timesClicked++;
-      save();
       break;
     }
   }
@@ -88,24 +69,17 @@ function handleProductClick(event) {
     productContainer.removeEventListener('click', handleProductClick);
   } else {
     renderProducts();
+    save();
   }
 }
 
-// function renderResults() {
-//   // let ul = document.querySelector('ul');
-//   for (let i = 0; i < products.length; i++) {
-//     let li = document.createElement('li');
-//     li.textContent = `${products[i].name} had ${products[i].timesSeen} views and was clicked ${products[i].timesClicked} times.`;
-//     // ul.appendChild(li);
-//   }
-// }
-
-let chartObj = null;
+// let chartObj = null;
 resultButton.addEventListener('click', function() {
-  chartObj = renderChart();
+  // chartObj = 
+  renderChart();
   // chartObj.data.datasets[0].data[0] = 10;
-  console.log(chartObj);
-  chartObj.update();
+  // console.log(chartObj);
+  // chartObj.update();
 });
 
 function renderChart() {
@@ -143,28 +117,6 @@ function renderChart() {
   });
 }
 
-
-new Product('banana', './images/banana.jpg'),
-new Product('bathroom', './images/bathroom.jpg'),
-new Product('boots', './images/boots.jpg'),
-new Product('breakfast', './images/breakfast.jpg'),
-new Product('bubblegum', './images/bubblegum.jpg'),
-new Product('chair', './images/chair.jpg'),
-new Product('cthulhu', './images/cthulhu.jpg'),
-new Product('dog-duck', './images/dog-duck.jpg'),
-new Product('dragon', './images/dragon.jpg'),
-new Product('pen', './images/pen.jpg'),
-new Product('pet-sweep', './images/pet-sweep.jpg'),
-new Product('scissors', './images/scissors.jpg'),
-new Product('shark', './images/shark.jpg'),
-new Product('sweep', './images/sweep.png'),
-new Product('tauntaun', './images/tauntaun.jpg'),
-new Product('unicorn', './images/unicorn.jpg'),
-new Product('water-can', './images/water-can.jpg'),
-new Product('wine-glass', './images/wine-glass.jpg'),
-
-
-
 renderProducts();
 
 productContainer.addEventListener('click', handleProductClick);
@@ -177,30 +129,31 @@ function save() {
   localStorage.setItem('chartData', valuesToStore);
 }
 
-// function load() {
-//   let rawData = localStorage.getItem('chartData');
-//   let chartObject = JSON.parse(rawData);
-//   if (!chartObject) { [
-//     new Product('banana', './images/banana.jpg'),
-//     new Product('bathroom', './images/bathroom.jpg'),
-//     new Product('boots', './images/boots.jpg'),
-//     new Product('breakfast', './images/breakfast.jpg'),
-//     new Product('bubblegum', './images/bubblegum.jpg'),
-//     new Product('chair', './images/chair.jpg'),
-//     new Product('cthulhu', './images/cthulhu.jpg'),
-//     new Product('dog-duck', './images/dog-duck.jpg'),
-//     new Product('dragon', './images/dragon.jpg'),
-//     new Product('pen', './images/pen.jpg'),
-//     new Product('pet-sweep', './images/pet-sweep.jpg'),
-//     new Product('scissors', './images/scissors.jpg'),
-//     new Product('shark', './images/shark.jpg'),
-//     new Product('sweep', './images/sweep.png'),
-//     new Product('tauntaun', './images/tauntaun.jpg'),
-//     new Product('unicorn', './images/unicorn.jpg'),
-//     new Product('water-can', './images/water-can.jpg'),
-//     new Product('wine-glass', './images/wine-glass.jpg'),
-//   ];
+function load() {
+  let rawData = localStorage.getItem('chartData');
+  let chartObject = JSON.parse(rawData);
+  if (!chartObject) { 
+    return [
+      new Product('banana', './images/banana.jpg'),
+      new Product('bathroom', './images/bathroom.jpg'),
+      new Product('boots', './images/boots.jpg'),
+      new Product('breakfast', './images/breakfast.jpg'),
+      new Product('bubblegum', './images/bubblegum.jpg'),
+      new Product('chair', './images/chair.jpg'),
+      new Product('cthulhu', './images/cthulhu.jpg'),
+      new Product('dog-duck', './images/dog-duck.jpg'),
+      new Product('dragon', './images/dragon.jpg'),
+      new Product('pen', './images/pen.jpg'),
+      new Product('pet-sweep', './images/pet-sweep.jpg'),
+      new Product('scissors', './images/scissors.jpg'),
+      new Product('shark', './images/shark.jpg'),
+      new Product('sweep', './images/sweep.png'),
+      new Product('tauntaun', './images/tauntaun.jpg'),
+      new Product('unicorn', './images/unicorn.jpg'),
+      new Product('water-can', './images/water-can.jpg'),
+      new Product('wine-glass', './images/wine-glass.jpg'),
+    ];
 
-//   }
-//   return chartObject;
-// }
+  }
+  return chartObject;
+}
